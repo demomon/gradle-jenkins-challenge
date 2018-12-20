366e537909e9a25e6861e2ff46311562ff60171e
Joost van der Griendt <joostvdg@gmail.com>
2018-12-20 14:07:51 +0100

    Initial content commit

---
+++ src/main/java/com/github/demomon/gradlejenkinschallenge/GradleJenkinsChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.github.demomon.gradlejenkinschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJenkinsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleJenkinsChallengeApplication.class, args);
+	}
+
+}
+
